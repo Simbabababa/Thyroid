7da464534f76594592ffb370019d184f89c6cfdb
yangxin <775953378@qq.com>
2022-11-05 09:29:03 +0800

    提交infer

---
+++ infer_transformer.py
@@ -0,0 +1,127 @@
+import os
+import numpy as np
+import torch
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+from utils import GradCAM, show_cam_on_image, center_crop_img
+from vit_model import vit_base_patch16_224
+from vit_model import vit_base_patch16_224_in21k as create_model
+
+class ReshapeTransform:
+    def __init__(self, model):
+        input_size = model.patch_embed.img_size
+        patch_size = model.patch_embed.patch_size
+        self.h = input_size[0] // patch_size[0]
+        self.w = input_size[1] // patch_size[1]
+
+    def __call__(self, x):
+        # remove cls token and reshape
+        # [batch_size, num_tokens, token_dim]
+        result = x[:, 1:, :].reshape(x.size(0),
+                                     self.h,
+                                     self.w,
+                                     x.size(2))
+
+        # Bring the channels to the first dimension,
+        # like in CNNs.
+        # [batch_size, H, W, C] -> [batch, C, H, W]
+        result = result.permute(0, 3, 1, 2)
+        return result
+
+def main():
+
+    # predict
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+    # load image
+    img_path = "dataset/2/1020162_x_13312_y_16384.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+
+    # create model
+    model = create_model(num_classes=6, has_logits=False).to(device)
+    # load model weights
+    model_weight_path = "weights/model-9.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+
+
+
+
+    # GradCAM
+    model = create_model(num_classes=6, has_logits=False)
+    weights_path = "weights/model-9.pth"
+    model.load_state_dict(torch.load(weights_path, map_location="cpu"))
+    # Since the final classification is done on the class token computed in the last attention block,
+    # the output will not be affected by the 14x14 channels in the last layer.
+    # The gradient of the output with respect to them, will be 0!
+    # We should chose any layer before the final attention block.
+    # 最后一层为class token，需要选择在此之前的层数作为梯度提取
+    target_layers = [model.blocks[-1].norm1]
+
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    # load image
+    # img_path = "dataset/1/1020153_x_0_y_9984.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path).convert('RGB')   # (121,180)
+    img = np.array(img, dtype=np.uint8)         # (180,121,3)
+    img = center_crop_img(img, 224)             # (224,224,3),统一裁剪为224 x 224的方形图片
+    # [C, H, W]
+    img_tensor = data_transform(img) # 转化为tensor并归一化
+
+    # expand batch dimension
+    # [C, H, W] -> [N, C, H, W]
+    input_tensor = torch.unsqueeze(img_tensor, dim=0)   # 扩展batch维度
+    cam = GradCAM(model=model,
+                  target_layers=target_layers,
+                  use_cuda=False,
+                  reshape_transform=ReshapeTransform(model))
+
+    target_category = 1
+
+
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)  # (1, 224, 224)
+
+    grayscale_cam = grayscale_cam[0, :]         # (224, 224)
+    visualization = show_cam_on_image(img / 255., grayscale_cam, use_rgb=True)
+    plt.imshow(visualization)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
